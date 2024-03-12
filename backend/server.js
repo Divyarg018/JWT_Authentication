b8ddb5ac3dc37f515c7548b0b0000d4abf399072
Divya R <kwrdivyaraj100@gmail.com>
2024-03-12 12:30:32 +0530

    CReated reuired files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://admin:k19F8kCpZphoDijn@cluster0.hubtpxn.mongodb.net/jwt_aa?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        app.listen(5000);
+        console.log("Database is connected! Listening to localhost 5000");
+    })
+    .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json);
 
-app.listen(5000, () => {
-    console.log("App started in port no 5000");
-});
+
+
+
+
+
+
+// k19F8kCpZphoDijn - mongodb password
