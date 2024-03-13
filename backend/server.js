96fe6c3f3d5552a107eea1dfb96865d9c99be789
Divya R <kwrdivyaraj100@gmail.com>
2024-03-13 14:52:07 +0530

    Getting user details

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ require('dotenv').config();
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
+
 mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.hubtpxn.mongodb.net/jwt_aa?retryWrites=true&w=majority&appName=Cluster0`)
-    .then(() => app.listen(5000))
-    .then(() => console.log("Database is connected! Listening to localhost 5000"))
-    .catch((err) => console.log(err));
+.then(() => app.listen(5000))
+.then(() => console.log("Database is connected! Listening to localhost 5000"))
+.catch((err) => console.log(err));
 
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
