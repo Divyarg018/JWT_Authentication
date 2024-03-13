bd7aac45ed2f60ac6856168db903f76fd0318981
Divya R <kwrdivyaraj100@gmail.com>
2024-03-13 12:12:33 +0530

    Adding Verification Of JWT Token With verifyToken and Adding GET HTTP Request for user

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const router = require("./routes/user_routes.js");
 
-mongoose.connect("mongodb+srv://admin:k19F8kCpZphoDijn@cluster0.hubtpxn.mongodb.net/jwt_aa?retryWrites=true&w=majority&appName=Cluster0")
+require('dotenv').config();
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.hubtpxn.mongodb.net/jwt_aa?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => app.listen(5000))
     .then(() => console.log("Database is connected! Listening to localhost 5000"))
     .catch((err) => console.log(err));
