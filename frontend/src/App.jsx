448274de9ba50b4b75761bd1c28838b20b3708b7
Divya R <kwrdivyaraj100@gmail.com>
2024-03-13 16:43:33 +0530

    Adding Routes in application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import React from "react";
 import './App.css';
 import Header from './components/Header';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-      <Header />
-    </div>
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<signup />} />
+          <Route path="/user" element={<Welcome />} />
+        </Routes>
+      </main>
+    </React.Fragment>
+
+
   );
 }
 
