95e5399d5154365bba524b831224a5df016b9377
Divya R <kwrdivyaraj100@gmail.com>
2024-03-13 16:12:42 +0530

    Installing react-router-dom packages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 
 function Header() {
+    const [value, setValue] = useState();
     return (
-        <div>Header</div>
+        <div>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant='h3'>MernAuth</Typography>
+                    <Box sx={{ marginLeft: "auto" }}>
+                        <Tabs
+                            indicatorColor='secondary'
+                            onChange={(e, val) => setValue(val)}
+                            value={value}
+                            textColor='inherit'
+                        >
+                            <Tab label="Login" />
+                            <Tab label="Signup" />
+                        </Tabs>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
