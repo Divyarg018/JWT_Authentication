4e4552533fdadbe4c175675bbe23b38134367c09
Divya R <kwrdivyaraj100@gmail.com>
2024-03-15 11:06:38 +0530

    Adding Protected Route (user)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/user" element={<Welcome />} />
+          {isLoggedIn && <Route path="/user" element={<Welcome />} />}
         </Routes>
       </main>
     </React.Fragment>
